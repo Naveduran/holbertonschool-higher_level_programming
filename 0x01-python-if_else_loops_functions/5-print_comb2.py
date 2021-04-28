205fff1be21233b8afcc3b71ea29126979f86809
Naveduran <naveduran@gmail.com>
2021-04-28 01:43:37 +0000

    using for loops and string format

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for a in range(0, 9):
-    for b in range(0, 9):
-        print("{}{}, ".format(a, b), end='')
-print("89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99")
+for a in range(00, 99):
+        print("{}, ".format(str(a).zfill(2)), end='')
+print("99")
