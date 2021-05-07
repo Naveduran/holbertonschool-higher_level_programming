550765db8c5acbb3c4bb49a601d9727d1c017dc7
Naveduran <naveduran@gmail.com>
2021-05-07 15:12:49 +0000

    type(element) == int

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        lenght = len(my_list)
-        while length:
-            print("{:d}".format(element)
-            length -= 1
+        my_list.reverse()
+        for e in my_list:
+            if type(e) == int:
+                print("{:d}".format(e))
