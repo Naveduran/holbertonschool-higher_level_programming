9973363b3c46ed562865dd82137db0b7d72b5b3b
Naveduran <naveduran@gmail.com>
2021-05-07 23:22:53 +0000

    adding tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        y = 0
+        if len(tuple_a) == 1:
+            y = tuple_a[0]
+        tuple_a = ((y),0)
+    if len(tuple_b) < 2:
+        y = 0
+        if len(tuple_b) == 1:
+            y = tuple_b[0]
+        tuple_b = ((y),0)
+    c = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return c
