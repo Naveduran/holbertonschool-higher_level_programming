4c7f7790fe2095dcdec94acff17a55f496e9df38
Naveduran <naveduran@gmail.com>
2021-05-07 20:07:30 +0000

    lenght of a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     l = len(matrix[0])
-    if l < 1:
-        print()
-    for i in range(l):
+    m = len(matrix)
+    for i in range(m):
         for j in range(l):
             print("{:d}".format(matrix[i][j]), end=' ')
         print()
