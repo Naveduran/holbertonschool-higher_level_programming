a48d98fae0eb3044e370d57a3138a15dbad6febe
Naveduran <naveduran@gmail.com>
2021-05-08 19:17:45 +0000

    squaring a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    l = [ele[:] for ele in matrix]
+    for i in range(len(l)):
+        for e in range(len(l[i])):
+            l[i][e] = l[i][e] * l[i][e]
+    return l
