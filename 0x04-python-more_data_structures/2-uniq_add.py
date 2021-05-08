46e71cd16c9f83e8159b66ac9ed991018ad6f25f
Naveduran <naveduran@gmail.com>
2021-05-08 21:41:41 +0000

    sorting to add unique numbers of a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,6 @@ def uniq_add(my_list=[]):
     my_list.sort()
     new = 0
     for i in range(len(my_list)):
-        if my_list[i] != my_list[i - 1]:
+        if i == 0 or my_list[i] != my_list[i - 1]:
             new += my_list[i]
     return new
