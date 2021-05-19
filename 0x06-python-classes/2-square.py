92dced1124fb2c774628d98c489ef5a02e5e344f
Naveduran <naveduran@gmail.com>
2021-05-19 06:46:50 +0000

    raise exception inside a class init

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,9 +5,8 @@
 class Square():
     ''' a square class '''
     def __init__(self, size=0):
-        try:
-            if type(size) == int:
-                self.__size = size
-            else:
-                raise Exception ValueError 
-        
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
