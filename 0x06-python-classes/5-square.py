f02cda75c89406d448466439b84e31c1c785dc1b
Naveduran <naveduran@gmail.com>
2021-05-19 08:17:16 +0000

    print a square with #

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,5 +3,30 @@
 
 
 class Square():
-    ''' an empty class '''
-    pass
+    ''' a square class '''
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
