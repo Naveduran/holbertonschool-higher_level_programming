ed32f5c964ba8fe3fbec14d8a2da7cb8efa20b05
Naveduran <naveduran@gmail.com>
2021-05-20 13:57:59 +0000

    ...

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
         self.print_symboL = '#'
 
@@ -24,6 +24,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
+        print("value = {}".format(type(value)))
         self.__width = value
 
     @property
@@ -36,6 +37,7 @@ class Rectangle:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
+        print("value = {}".format(type(value)))
         self.__height = value
 
     def area(self):
