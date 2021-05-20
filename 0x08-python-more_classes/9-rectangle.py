510d52d8528328a2a1850a54736961a8908b2f0f
Naveduran <naveduran@gmail.com>
2021-05-20 11:13:22 +0000

    class method

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -65,6 +65,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) != Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
@@ -77,5 +78,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        square = Rectangle(size, size)
+        square = cls(size, size)
         return square
