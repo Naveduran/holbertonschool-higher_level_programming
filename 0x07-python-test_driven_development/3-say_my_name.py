133fcdd1217c59ed9dd5807391c43415617af58f
Naveduran <naveduran@gmail.com>
2021-05-25 13:40:02 +0000

    Good night

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' contains a function that prints My name is <first name> <last name> '''
+
+
+def say_my_name(first_name, last_name=""):
+    ''' function that prints My name is <first name> <last name> '''
+
+    if type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    if last_name == "":
+        print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
