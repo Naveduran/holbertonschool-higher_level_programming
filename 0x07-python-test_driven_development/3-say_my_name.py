04addc2feffc26f5e04c260610bf28ebd55a6118
Naveduran <naveduran@gmail.com>
2021-05-26 05:34:41 +0000

    space after first name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,6 +10,6 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) != str:
         raise TypeError('last_name must be a string')
     if last_name == "":
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(first_name))
     else:
         print("My name is {} {}".format(first_name, last_name))
