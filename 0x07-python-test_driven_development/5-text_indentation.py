3cbafc09392bcc9ee384baba7a68c9350abe36c7
Naveduran <naveduran@gmail.com>
2021-05-26 07:44:45 +0000

    new lines fixed

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,23 +14,23 @@ def text_indentation(text):
     if type(text) != str:
         raise TypeError('text must be a string')
 
+    text = text.strip()
     new_text = ''
 
     for char in text:
-        new_text += char
+        if char != '\n':
+            new_text += char
         if char == '?' or char == ':' or char == '.':
-            new_text += '\n\n'
+            new_text += '\n'
 
     list_of_lines = new_text.split('\n')
 
-    for i in range(len(list_of_lines)):
-        if list_of_lines[i].startswith(' ') or list_of_lines[i].endswith(' '):
-            list_of_lines[i] = list_of_lines[i].strip(' ')
-        if list_of_lines[i] == '\n' and list_of_lines[i + 1] == '\n':
-            list_of_lines.pop(i)
-            i -= 1
-
-    list_of_lines = list_of_lines[:-1]
+    new_list = []
 
     for line in list_of_lines:
-        print(line)
+        if line != '':
+            item = line.strip(' ')
+            print(item)
+            last_c = item[-1]
+            if last_c == '?' or last_c == ':' or last_c == '.':
+                print()
