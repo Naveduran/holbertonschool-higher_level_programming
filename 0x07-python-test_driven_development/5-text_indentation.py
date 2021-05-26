97c6ea56c48b0d7d2c1b3389bcb39d7fc3218322
Naveduran <naveduran@gmail.com>
2021-05-26 05:49:51 +0000

    correction of sed bad replacement

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,17 +11,17 @@ def indentation_indentation(text):
     after each of these characters: ., ? and :
     '''
 
-    if type(indentation) != str:
-        raise TypeError('indentation must be a string')
+    if type(text) != str:
+        raise TypeError('text must be a string')
 
-    new_indentation = ''
+    new_text = ''
 
-    for char in indentation:
-        new_indentation += char
+    for char in text:
+        new_text += char
         if char == '?' or char == ':' or char == '.':
-            new_indentation += '\n\n'
+            new_text += '\n\n'
 
-    list_of_lines = new_indentation.split('\n')
+    list_of_lines = new_text.split('\n')
 
     for i in range(len(list_of_lines)):
         if list_of_lines[i].startswith(' ') or list_of_lines[i].endswith(' '):
