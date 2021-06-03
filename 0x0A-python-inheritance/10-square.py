d2ec1d83dfdb6943aede382022f6b9e508157441
Naveduran <naveduran@gmail.com>
2021-06-03 04:15:22 +0000

    calling the parent method

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module, 10° task'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' inherits from Rectangle'''
+
+    def __init__(self, size):
+        '''inicialization of values'''
+        Rectangle.__init__(self, size, size)
