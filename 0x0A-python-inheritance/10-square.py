5f70d43a581aeb48d7fd08957722845063a060c5
Naveduran <naveduran@gmail.com>
2021-06-03 04:17:47 +0000

    calling the parent method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,7 +5,6 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     ''' inherits from Rectangle'''
-
     def __init__(self, size):
         '''inicialization of values'''
         Rectangle.__init__(self, size, size)
