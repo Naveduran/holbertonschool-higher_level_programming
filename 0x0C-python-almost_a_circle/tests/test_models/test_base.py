72f46eadf64096a2ff2a6b89c822162d95cf6560
Naveduran <naveduran@gmail.com>
2021-06-05 14:48:17 +0000

    test base, proyect general requirements and documentation

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,105 +1,122 @@
 #!/usr/bin/python3
-'''unit test for base and its functions'''
-import os
-import unittest
+# unit test for base.py script and requirements of the general proyect
+
+import unittest  # allows to assert
+import os  # allows to run bash commands
+
+# scripts to test:
 from models.base import Base
+from models.rectangle import Rectangle
+# from models.square import Square
 
 
-class TestFunction(unittest.TestCase):
-    '''unit test for class Base'''
+class Test_Base(unittest.TestCase):
+    # class to test the unit models.base.py
 
-    word = "Betty"
-    string = "Betty Holberton"
-    pharagraph = '''Lorem Ipsum is simply dummy text of the printing and\
-        typesetting industry. Lorem Ipsum has been the	industry's standard\
-        dummy text ever since the 1500s, when an unknown printer took a galley\
-        of type and scrambled it to make a type specimen book. It has survived\
-        not only five centuries, but also the leap into electronic typesetting\
-        ,remaining essentially unchanged. It was popularised in the 1960s with\
-        the release of Letraset sheets containing Lorem Ipsum passages, and\
-        more recently with desktop publishing software like Aldus PageMaker\
-        including versions of Lorem Ipsum.'''
+    word = 'Betty'
+    string = 'Betty Holberton'
     positive = 5
-    negatve = -5
+    negatve = 0 - 5
     zero = 0
     nothing = None
-    dictionary = {'Key0': 0, 'Key1': 1, 'Key2': 2, 'Key3': 2, 'Key4': 3, }
+    dictionary = {'Key0': 0, 'Key1': 1, 'Key2': 2, 'Key3': 3, 'Key4': 4}
     list = [1, 2, 3, 4, 5]
     boolean = True
 
-    def test_function(self):
-        '''test for function___'''
+    def test_Base(self):
+        # test for class Base, task 0
+
+        a = Base()
+# ?        self.assertEqual(a.__nb_objects, 1)
+        b = Base()
+        c = Base()
+        d = Base(12)
+        e = Base()
+
+        self.assertEqual(a.id, 1)
+        self.assertEqual(b.id, 2)
+        self.assertEqual(c.id, 3)
+        self.assertEqual(d.id, 12)
+        self.assertEqual(e.id, 4)
+# ?        self.assertEqual(__nb_objects, 4)
+
+        self.assertTrue(type(a), Base)
+        self.assertIsInstance(a, Base)
+
+    def doc_of_funcs(self):
+
+        # documentation of functions
+
+        # base has a method to_json_string(list_dictionaries)
+        # to_json_string devuelve un string
+
+        # base has a static method from_json_string(json_string)
+        # from_json_string devuelve una lista
+
+        # base has a class method create(cls, **dictionary)
+        # returns an object with all the attributes setted
+
+        # base has a class method load_from_file(cls)
+        # returns a list of instances
+
+        # base has a class method save_to_file_csv(cls, list_objs)
+        # do
+
+        # base has a class method load_from_file_csv(cls):
+        # do
+
+        pass
+
+    def whole_project_requirements(self):
+
+        # existence of all files and directories in the correct location
+
+        self.assertTrue(os.path.isfile('README.md'))
+
+        self.assertTrue(os.path.isdir('models'))
+
+        self.assertTrue(os.path.isfile('./models/__init__.py'))
+        self.assertTrue(os.path.isfile('./models/base.py'))
+        self.assertTrue(os.path.isfile('./models/rectangle.py'))
+        self.assertTrue(os.path.isfile('./models/square.py'))
+
+        self.assertTrue(os.path.isdir('tests'))
+        self.assertTrue(os.path.isdir('./tests/test_models'))
+
+        self.assertTrue(os.path.isfile('./tests/test_models/__init__.py'))
+        self.assertTrue(os.path.isfile('./tests/test_models/test_base.py'))
+        # self.assertTrue(os.path.isfile('./tests/test_models/test_rectangle.py'))
+        # self.assertTrue(os.path.isfile('./tests/test_models/test_square.py'))
 
         # files are executable
+        self.assertTrue(os.access('./models/base.py', os.X_OK))
+        self.assertTrue(os.access('./models/rectangle.py', os.X_OK))
+        self.assertTrue(os.access('./models/square.py', os.X_OK))
 
-        assertTrue(os.access('./models/base.py', os.X_OK))
+        # first and last line OF BASE
+        with open('./models/base.py') as f:
+            first = f.readline()
+            last = f.read()[-1]
+            print("Last :" + last, end='')
+
+        self.assertTrue(first == '#!/usr/bin/python3\n')
+        self.assertTrue(last == '\n')
 
         # pep8
+        self.assertEqual(os.system('pep8 --count ./models/base.py'), 0)
+        self.assertEqual(os.system('pep8 --count ./models/rectangle.py'), 0)
+        self.assertEqual(os.system('pep8 --count ./models/square.py'), 0)
 
-        assertTrue(os.system('pep8 --count base.py'))
+        # documentation of modules and classes
+        self.assertTrue(len('base'.__doc__) > 8)
+        self.assertTrue(len(Base.__doc__) > 8)
 
-        # documentation of classes and functions
+        self.assertTrue(len('rectangle'.__doc__) > 8)
+        self.assertTrue(len(Rectangle.__doc__) > 8)
 
-        assertTrue(os.system(len('base'.__doc__) > 10))
-        assertTrue(os.system(len(Base.__doc__) > 10))
-
-        # first and last line
-
-        with open('base.py') as f:
-            first = f.read()[-1]
-            last = f.readline()
-
-        assertTrue(first == '\n\n')
-        assertTrue(last == '#!/usr/bin/python3\n')
-
-        # base has a private attribute __nb_objects and its equal to 0
-
-        assertTrue(__nb_objects in dir(Base))
-
-        # base has a method __init__(self, id=None)
-
-        assertTrue(__init__ in dir(Base))
-
-        # __init(none) => nb += 1, public_id = nb
-
-        # __init(!=none) => public_id = id
-
-        u = base()
-        assertTrue(u.__nb_objects, 0)
-        u.id
-
-    def test_raise_errors(self):
-        '''test output for different type of input values'''
-        assertRaises(ValueError, function, arg)
+        # self.assertTrue(len('square'.__doc__) > 8)
+        # self.assertTrue(len(Square.__doc__) > 8)
 
 
-# base has a method to_json_string(list_dictionaries)
-# to_json_string devuelve un string
-
-# base has a static method from_json_string(json_string)
-# from_json_string devuelve una lista
-
-# base has a class method create(cls, **dictionary)
-# returns an object with all the attributes setted
-
-# base has a class method load_from_file(cls)
-# returns a list of instances
-
-# ___
-# base has a class method save_to_file_csv(cls, list_objs)
-# do
-
-# base has a class method load_from_file_csv(cls):
-# do
-
-
-'''
-assertTrue(x)
-assertFalse(x)
-assertIs(a, b)
-assertIsNot(a, b)
-assertIn(a, b)
-assertNotIn(a, b)
-assertIsInstance(a, b)
-assertNotIsInstance(a, b)
-'''
+if __name__ == '__main__':
+    unittest.main()
