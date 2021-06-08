96cf250fd2d3886741dff3d6c0738660ca8abc87
Naveduran <naveduran@gmail.com>
2021-06-08 15:22:32 +0000

    save to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,12 +33,13 @@ class Base():
         filename = str('{}.json'.format(cls.__name__))
         dict_of_obj = {}
         list_of_dicts = []
-        for object in list_objs:
-            dict_of_obj = object.to_dictionary()
-            list_of_dicts.append(dict_of_obj)
-        string = cls.to_json_string(list_of_dicts)
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(string)
+        if list_objs:
+            for object in list_objs:
+                dict_of_obj = object.to_dictionary()
+                list_of_dicts.append(dict_of_obj)
+                string = cls.to_json_string(list_of_dicts)
+                with open(filename, 'w', encoding='utf-8') as file:
+                    file.write(string)
 
     @staticmethod
     def from_json_string(json_string):
