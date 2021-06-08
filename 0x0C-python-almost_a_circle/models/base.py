38a0cd707edb7253fcb25d0c618ac959d1a45799
Naveduran <naveduran@gmail.com>
2021-06-08 15:15:13 +0000

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,9 +43,10 @@ class Base():
     @staticmethod
     def from_json_string(json_string):
         ''' returns the list of the JSON string representation json_string '''
-        if json_string is None or json_string == []:
+        if json_string is None or len(json_string) < 0:
             return []
-        return json.loads(json_string)
+        result = json.loads(json_string)
+        return result
 
     @classmethod
     def create(cls, **dictionary):
