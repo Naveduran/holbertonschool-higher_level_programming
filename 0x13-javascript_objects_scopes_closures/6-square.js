b2805b814a302769aca0b3b542ade9530997091f
Natalia <naveduran@gmail.com>
2021-08-03 21:51:43 -0500

    include If c is undefined, use the character X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (letter='X') {
+    for (let step = 0; step < this.height; step++) {
+      console.log(letter.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
