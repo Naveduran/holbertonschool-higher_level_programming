77c7e27fb7efe2ad4ffd37a37b236d1bf3afdcad
Natalia <naveduran@gmail.com>
2021-08-03 22:08:06 -0500

    use the super class print

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
-
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
@@ -10,5 +9,4 @@ class Square extends Rectangle {
     super.print(c);
   }
 }
-
 module.exports = Square;
