e48ecb7a7ef0581352cb4fc658243b397664f948
Naveduran <naveduran@gmail.com>
2021-08-09 04:27:17 +0000

    [Fixed] select * replaced for specific variables

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,21 +7,18 @@ from sys import argv
 def Filter_States(username, password, db_name):
     """lists all states with a name starting with N"""
 
-    # Open database connection
     db = MySQLdb.connect(user=username,
                          passwd=password,
                          db=db_name)
 
-    # prepare a cursor object using cursor() method
     cur = db.cursor()
 
-    # execute SQL query using execute() method.
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT states.id, states.name FROM states WHERE\
+        name LIKE 'N%' ORDER BY states.id ASC;")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
 
-    # close cursor and connection
     cur.close()
     db.close()
 
