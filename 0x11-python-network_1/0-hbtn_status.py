0f9a0ccbf5e5da4a5d9c9d65f1dd4ad8efaabd5e
Naveduran <naveduran@gmail.com>
2021-08-20 23:49:44 +0000

    [fix] decoding content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # fetches https://intranet.hbtn.io/status
 
-import urllib.request
 
 if __name__ == "__main__":
     # fetches a web page
+    import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        the_page = response.read()
-    content = str(the_page).split("'")[1]
+        page = response.read()
     str = """Body response:\n\t- type: {}\n\t- content: {}
-\t- utf8 content: {}""".format(type(the_page), the_page, content)
+\t- utf8 content: {}""".format(type(page), page,
+                               page.decode(encoding='utf-8'))
     print(str)
