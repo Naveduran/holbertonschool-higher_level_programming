a91d9e167cf6f00b0b8a0e25d14ceb9ee5987e43
Naveduran <naveduran@gmail.com>
2021-08-20 23:33:27 +0000

    [add] fetcher

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 # fetches https://intranet.hbtn.io/status
+
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
+    # fetches a web page
     request = Request('https://intranet.hbtn.io/status')
     response = urlopen(request)
     with urlopen(request) as response:
         the_page = response.read()
-        content = str(the_page).split("'")[1]
+    content = str(the_page).split("'")[1]
     str = """Body response:
     - type: {}
     - content: {}
