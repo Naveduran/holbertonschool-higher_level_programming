0eb5c7827cdfebe0512048b863cda9d410cf26b3
Naveduran <naveduran@gmail.com>
2021-08-20 23:43:29 +0000

    [fix] use tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         the_page = response.read()
     content = str(the_page).split("'")[1]
-    str = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".format(type(the_page), the_page, content)
+    str = """Body response:\n\t- type: {}\n\t- content: {}
+\t- utf8 content: {}""".format(type(the_page), the_page, content)
     print(str)
