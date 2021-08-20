fa49925f852629132cb4e2f8e83758cfa4e3c406
Naveduran <naveduran@gmail.com>
2021-08-20 23:26:55 +0000

    [add] fetcher

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# fetches https://intranet.hbtn.io/status
+from urllib.request import Request, urlopen
+
+if __name__ == "__main__":
+    request = Request('https://intranet.hbtn.io/status')
+    response = urlopen(request)
+    with urlopen(request) as response:
+        the_page = response.read()
+        content = str(the_page).split("'")[1]
+    str = """Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(the_page), the_page, content)
+    print(str)
