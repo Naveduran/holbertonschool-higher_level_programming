78da7df828d538d031cef64582a83a29fe339109
Naveduran <naveduran@gmail.com>
2021-08-20 23:57:29 +0000

    [fix] comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# fetches https://intranet.hbtn.io/status
+"""
+Fetches https://intranet.hbtn.io/status
+"""
 
 
 if __name__ == "__main__":
-    # fetches a web page
     import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         page = response.read()
-    str = """Body response:\n\t- type: {}\n\t- content: {}
-\t- utf8 content: {}""".format(type(page), page,
-                               page.decode(encoding='utf-8'))
-    print(str)
+    print("Body response:\n\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode(encoding='utf-8')))
