01e61ab548c4280fb7158a3b8fa0d4724bd1ccea
Naveduran <naveduran@gmail.com>
2021-08-22 17:52:51 +0000

    [Add] allow execution of the module at import

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # Fetches a web page whit the module requests
-if __name__ == "__main__":
-    import requests
-
-    r = requests.get('https://intranet.hbtn.io/status')
-
-    print("Body response:\n\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+import requests
+r = requests.get('https://intranet.hbtn.io/status')
+print("Body response:\n\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
