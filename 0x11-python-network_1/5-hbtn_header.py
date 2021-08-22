e54731a29ca10fbb1d7d95d92d32a2d1ca423c9f
Naveduran <naveduran@gmail.com>
2021-08-22 19:29:52 +0000

    [Add] don't execute it on import

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Fetch a page and gets an specific header
-import requests
-from sys import argv
-r = requests.get(argv[1])
-print(r.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
