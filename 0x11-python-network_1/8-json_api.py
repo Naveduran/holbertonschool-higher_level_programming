664d57e0137c9fda9b01ffc46a00570afd17a3b4
Naveduran <naveduran@gmail.com>
2021-08-23 02:17:32 +0000

    POST a letter and get a json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+POST a letter and receive response
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    url = 'http://c0cd6e92.hbtn-cod.io:5000/search_user'
+
+    q = ""
+    if len(argv) >= 2:
+        q = argv[1]
+
+    data = {'q': q}
+
+    r = requests.post(url, data)
+
+    if r.headers.get('Content-Type') == 'application/json':
+        json = r.json()
+        if json:
+            print("[{}] {}".format(json.get('id'), json.get('name')))
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
