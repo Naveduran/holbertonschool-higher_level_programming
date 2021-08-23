0a756ca35aae28cb61189b440b5b3e6d2af050c1
Naveduran <naveduran@gmail.com>
2021-08-23 08:20:30 +0000

    [Fix] using try and except

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,9 @@ if __name__ == '__main__':
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2],
                                                               argv[1])
-    data = {'per_page': 10}
     try:
-        r = requests.get(url, data)
-        for i in range(len(r.json())):
+        r = requests.get(url)
+        for i in range(10):
             sha = r.json()[i].get('sha')
             name = r.json()[i].get('commit').get('author').get('name')
             print("{}: {}".format(sha, name))
