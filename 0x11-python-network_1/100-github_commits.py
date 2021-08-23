76a790adc21aa44e8250d260d89bf55a1693aa15
Naveduran <naveduran@gmail.com>
2021-08-23 08:11:18 +0000

    [Fix] Less than 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,9 @@ if __name__ == '__main__':
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2],
                                                               argv[1])
-
     data = {'per_page': 10}
     r = requests.get(url, data)
-    for i in range(10):
+    for i in range(len(r.json())):
         sha = r.json()[i].get('sha')
         name = r.json()[i].get('commit').get('author').get('name')
         print("{}: {}".format(sha, name))
