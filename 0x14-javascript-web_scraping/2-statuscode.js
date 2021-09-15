92e3a6d0e3e911e9e5d154b05afb95e61161fbf5
Naveduran <naveduran@gmail.com>
2021-09-15 04:36:05 +0000

    [Add] use module request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,6 @@ const request = require('request');
 
 const URL = process.argv[2];
 request(URL, function (err, response) {
+  if (err) { console.error(err); }
   console.log('code:', response.statusCode);
 });
