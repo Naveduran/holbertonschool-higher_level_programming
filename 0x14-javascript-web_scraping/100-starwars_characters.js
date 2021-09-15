712467417fd37f9fc8fa12970e073e4c935da569
Naveduran <naveduran@gmail.com>
2021-09-15 06:05:51 +0000

    [Add] double requesting :3

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// prints the characters present in a Star Wars Movie
+const request = require('request');
+
+const movieId = process.argv[2];
+const URL = 'https://swapi-api.hbtn.io/api/films/'.concat(movieId);
+
+request(URL, function (err, response, body) {
+  if (err) { console.error(err); }
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; characters[i]; i++) {
+    request(characters[i], function (err, response, body) {
+      if (err) { console.error(err); }
+      const name = JSON.parse(body).name;
+      console.log(name);
+    });
+  }
+});
