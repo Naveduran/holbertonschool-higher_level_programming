2ea190e5f6bbcefd4dfeb429c6c5e5ee265f4a34
Natalia <naveduran@gmail.com>
2021-09-22 23:57:08 -0500

    [Add] get a JSON from a get request

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,10 +1,6 @@
-// fetches and lists the title for all movies of Starwars
+// fetches and lists the character names for a movie of Starwars
 $(document).ready(function () {
-  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data, textStatus) {
-    const values = Object.values(data);
-    for (let item = 0; values[item]; item++) {
-      alert(values[item]);
-      // $('div#character').append(name);
-    }
+  $.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+    $('div#character').text(data.name);
   });
 });
