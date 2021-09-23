80cadef53b4fdaa321f137779d402e8bbd4de02b
Natalia <naveduran@gmail.com>
2021-09-23 00:14:10 -0500

    [Delete] debugging prints

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,10 @@
 // fetches and lists the title for all movies of Starwars
 $(document).ready(function () {
   $.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-    console.log(data);
     for (const item of data.results) {
       tittle = item.title;
       let element = "<li>" + tittle + "</li>"
-      console.log(element);
-      $dino2 ('ul').append(element);
+      $('ul').append(element);
     }
   });
 });
